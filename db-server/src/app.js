f2d9403b44ccbc3d5568f9b82b2a8d580e4270ff
John Brehm <cooljohnny3@gmail.com>
2018-06-29 09:54:58 -0700

    Start password confirm check

--- db-server/src/app.js
+++ db-server/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // express-session
 app.use(session({
@@ -45,6 +45,7 @@ const user_pool = mysql.createPool({
     database: 'users'
 });
 
+// TODO sort by column
 app.get('/', (req, res) => {
     let pageNum = 0;
     let start;
--- db-server/src/public/js/main.js
+++ db-server/src/public/js/main.js
@@ -5,3 +5,16 @@ $(document).ready(() => {
         window.location = '/view?id=' + $(this).attr('id');
     });
 });
+
+function passConf() {
+    alert("TEST!");
+    let pass1 = document.getElementById("pass").value;
+    let pass2 = document.getElementById("passConf").value;
+    if (pass1 == pass2) {
+        alert("Paswords Match!");
+    } else {
+        document.getElementById("pass1").style.borderColor = "#E34234";
+        document.getElementById("pass2").style.borderColor = "#E34234";
+        alert("Paswords Don't Match!");
+    }
+}
